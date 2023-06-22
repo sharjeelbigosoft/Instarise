ccae0bc56cbbaf19b30da7262bd0b56d69a39ad1
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-22 11:13:38 +0500

    Centered the description section of slogan

--- src/components/Slogan.tsx
+++ src/components/Slogan.tsx
@@ -6,7 +6,7 @@ export default function Slogan() {
       <div className='flex justify-center my-36'>
         <div className='grid lg:w-1/2 justify-self-center justify-center'>
           <h1 className='capitalize text-7xl font-normal'>we grow your brand on a budget friendly solution!</h1>
-          <p className='capitalize text-md text-gray-500 my-6 w-1/2'>high-standard, high-performing work that don't cost a fortune.</p>
+          <p className='capitalize text-md text-gray-500 my-6 w-1/3 text-center justify-self-center'>high-standard, high-performing work that don't cost a fortune.</p>
         </div>
       </div>
     </>
