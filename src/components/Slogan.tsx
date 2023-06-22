0434570a0d929498018b28a252175c16044b8b35
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-22 11:10:27 +0500

    Added slogan of the website. Description needed to be centered.

---
+++ src/components/Slogan.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Slogan() {
+  return (
+    <>
+      <div className='flex justify-center my-36'>
+        <div className='grid lg:w-1/2 justify-self-center justify-center'>
+          <h1 className='capitalize text-7xl font-normal'>we grow your brand on a budget friendly solution!</h1>
+          <p className='capitalize text-md text-gray-500 my-6 w-1/2'>high-standard, high-performing work that don't cost a fortune.</p>
+        </div>
+      </div>
+    </>
+  )
+}
