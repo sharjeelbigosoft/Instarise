d018d1e73522b030f35b95be7b619965796b1b59
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-22 11:08:16 +0500

    Take header at front by making background white, so the scrolling components go into background.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     // <!-- Fixed header -->
-    <div className="fixed flex justify-center mt-8 w-full top-0">
+    <div className="fixed flex justify-center mt-8 w-full top-0 bg-white">
       <div className='flex justify-between py-4 px-12 w-4/5 shadow-md'>
         <div className="flex items-center">
             <h1 className="uppercase font-medium">instarise</h1>
