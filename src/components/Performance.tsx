b820ed9e8e35f9c3e7554479403dbb643cdde206
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-22 11:50:36 +0500

    Added performance numbers.

---
+++ src/components/Performance.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Performance() {
+  return (
+    <>
+        <div className='flex justify-center'>
+            <div className='w-1/5 p-10'>
+                <h3 className='font-medium text-5xl pb-4'>96%</h3>
+                <h4 className='w-4/5'>Those who view product/service videos are 96% more likely to buy</h4>
+            </div>
+            <div className='w-1/5 p-10 border-r-2 border-l-2'>
+                <h3 className='font-medium text-5xl pb-4'>#1</h3>
+                <h4 className='w-4/5'>Rated #1 in agencies who help small businesses grow. Among the worlds top 3%</h4>
+            </div>
+            <div className='w-1/4 p-10'>
+                <h3 className='font-medium text-5xl pb-4'>78%</h3>
+                <h4 className='w-4/5'>78% of small businesses don't have the money to compete with big business top-tier talent. 90% of them don't know where to start...</h4>
+            </div>
+        </div>
+    </>
+  )
+}
