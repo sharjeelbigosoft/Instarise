777a49aa5944f2d48f0cafd478a5d2239a1354ff
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-22 10:48:15 +0500

    Header of the website

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    // <!-- Fixed header -->
+    <div className="flex justify-center mt-8">
+      <div className='flex justify-between py-4 px-12 w-4/5 shadow-md'>
+        <div className="flex items-center">
+            <h1 className="uppercase font-medium">instarise</h1>
+        </div>
+        <div>
+            <button className="bg-black text-white py-2 px-4 rounded-lg">Book a Call</button>
+        </div>
+      </div>
+    </div>
+  )
+}
