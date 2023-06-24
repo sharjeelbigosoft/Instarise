71676cd8dcaed3db4a6d74eabd7e0e6c804617cb
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-24 17:13:54 +0500

    Added Freelancer's section mentioning our services.

---
+++ src/components/Reliability.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Reliability() {
+  return (
+    <>
+        <div className='bg-black text-white flex justify-center py-24'>
+            <div className='lg:w-1/2 space-y-8'>
+                <h3 className='text-4xl lg:w-9/12 justify-self-center leading-relaxed'>Frustrated About Unreliable Freelancers, But Don't Have the Budget for an In-House</h3>
+                <h6 className='lg:w-7/12 text-zinc-400'>We replace unreliable freelancers and expensive agencies you can't afford. For a flat fee, we take the pain out of hiring ineffective creatives from platforms like Fiverr.</h6>
+                <h6 className=' text-zinc-400'>Lucky you, we fix that.</h6>
+                <div className='flex justify-between'>
+                    <button className='rounded-3xl text-sm text-white py-1 px-4 bg-gray-600 text-black'>Graphic & Web Design</button>
+                    <button className='rounded-3xl text-sm text-white py-1 px-4 bg-gray-600 text-black'>Social Media & Advertising</button>
+                    <button className='rounded-3xl text-sm text-white py-1 px-4 bg-gray-600 text-black'>UGC | Video Editing</button>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
