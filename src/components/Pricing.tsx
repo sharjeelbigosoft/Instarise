3a8ae7ac67fc8d74e00f08ca1eeb41f819b2e3db
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-25 20:44:31 +0500

    Customized pricing offering.

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -20,7 +20,7 @@ export default function Pricing() {
             <p>5 Stories per month</p>
             <p>Project Manager</p>
             <p>Jr. Creative designer</p>
-            <button className='px-4 border border-purple-400 rounded-lg hover:text-white hover:bg-purple-400pb-4  py-2 absolute bottom-1.5' >Book a Call</button>
+            <button className='w-[80%] px-4 border border-purple-400 rounded-lg hover:text-white hover:bg-purple-400 py-2 absolute bottom-1.5' >Book a Call</button>
           </div>
 
           <div className='relative lg:w-1/5 px-6 py-6 h-[120%] space-y-3'>
@@ -36,7 +36,7 @@ export default function Pricing() {
               <p>Basic Strategy | Hashtag | SEO</p>
               <p>Jr. Creative designer</p>
             </div>
-            <button className='px-4 border border-purple-400 rounded-lg hover:text-white hover:bg-purple-400pb-4  py-2 absolute bottom-1.5' >Book a Call</button>
+            <button className='w-[80%] px-4 border border-purple-400 rounded-lg hover:text-white hover:bg-purple-400 py-2 absolute bottom-1.5' >Book a Call</button>
           </div>
 
           <div className='relative lg:w-1/5 px-6 py-6 h-[120%] space-y-3'>
@@ -51,10 +51,62 @@ export default function Pricing() {
             <p>Basic Strategy | Hashtag | SEO</p>
             <p>Branding Strategy (Basic)</p>
             <p>Boost Incl</p>
-            <button className='px-4 border border-purple-400 rounded-lg hover:text-white hover:bg-purple-400pb-4  py-2 absolute bottom-1.5' >Get 3 Days free Trial</button>
+            <button className='w-[80%] px-4 border border-purple-400 rounded-lg hover:text-white hover:bg-purple-400 py-2 absolute bottom-1.5' >Get 3 Days free Trial</button>
           </div>
 
           <div className='lg:w-1/5 px-6 py-6'></div>
+        </div>
+      </div>
+
+
+      <div className='display flex justify-center my-6'>
+        <div className='w-3/5'>
+          <h3 className='font-medium text-lg'>Customize your plan with perks that make a difference</h3>
+          <div className='w-full flex justify-between p-10 border border-black '>
+            <div>
+              <p className='font-medium'>Time to Scale!</p>
+              <p className='font-medium'>From <span className='text-5xl'>$3,799</span> /month</p>
+            </div>
+            <div className='w-7/12'>
+              <p className=''>For fast-moving businesses in need of a design & marketing team to scale-up even faster.</p>
+              <div className='flex justify-center'>
+                {/* Logos */}
+                <div>
+
+                </div>
+                <ul className='list-disc '>
+                  <li>Social Media</li>
+                  <li>Website</li>
+                  <li>SEO</li>
+                  <li>FB Ads | Google Ads</li>
+                </ul>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex justify-center my-10'>
+        <div className='w-3/5 flex justify-around'>
+          <div className='w-1/3 px-6 space-y-2'>
+            <h4 className='font-medium'>Coordinate each step</h4>
+            <p className='text-sm text-slate-500'>The annual cost of a full-time, top quality designer now exceeds $84,000. Subscribe for less than a fraction of this!</p>
+          </div>
+          <ul className='w-1/3 px-6 space-y-2 text-sm font-medium list-image-{url(https://vhv.rs/dpng/d/409-4094705_blue-tick-png-transparent-png-blue-tick-white.png)}'>
+            <li>UGC videos</li>
+            <li>Brand Identity</li>
+            <li>Attractive UI/UX</li>
+            <li>Packaging Design</li>
+            <li>Organic & Paid Growth</li>
+          </ul>
+          <ul className='w-1/3 px-6 space-y-2 text-sm font-medium list-image-{url(htvhttps://.rs/dpng/d/409-4094705_blue-tick-png-transparent-png-blue-tick-white.png)}'>
+            <li>$1.80 Community Engagement</li>
+            <li>Video Editing</li>
+            <li>Content Creation</li>
+            <li>Unboxing Videos</li>
+            <li>Product Model Production</li>
+          </ul>
         </div>
       </div>
     </>
