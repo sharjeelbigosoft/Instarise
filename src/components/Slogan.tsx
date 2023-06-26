63807b23eec96c3c92811f344fbefae3d4748180
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-26 12:12:44 +0500

    Adjusting start of website from the fixed header.

--- src/components/Slogan.tsx
+++ src/components/Slogan.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Slogan() {
   return (
     <>
-      <div className='flex justify-center my-36'>
+      <div className='flex justify-center mt-36'>
         <div className='grid lg:w-1/2 justify-self-center justify-center'>
           <h1 className='capitalize text-7xl font-normal'>we grow your brand on a budget friendly solution!</h1>
           <p className='capitalize text-md text-gray-500 my-6 w-1/3 text-center justify-self-center'>high-standard, high-performing work that don't cost a fortune.</p>
