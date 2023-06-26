f4af46a91e51485bec43cd28da887f776f092812
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-26 15:38:55 +0500

    Slogan component made responsive

--- src/components/Slogan.tsx
+++ src/components/Slogan.tsx
@@ -4,8 +4,8 @@ export default function Slogan() {
   return (
     <>
       <div className='flex justify-center mt-36'>
-        <div className='grid lg:w-1/2 justify-self-center justify-center'>
-          <h1 className='capitalize text-7xl font-normal'>we grow your brand on a budget friendly solution!</h1>
+        <div className='grid lg:w-1/2 justify-center'>
+          <h1 className='max-lg:text-center capitalize text-7xl font-normal'>we grow your brand on a budget friendly solution!</h1>
           <p className='capitalize text-md text-gray-500 my-6 w-1/3 text-center justify-self-center'>high-standard, high-performing work that don't cost a fortune.</p>
         </div>
       </div>
