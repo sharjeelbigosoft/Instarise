a70155e25d7f224ec6b195d1662e650075c84d28
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-26 16:06:35 +0500

    Performance component made responsive

--- src/components/Performance.tsx
+++ src/components/Performance.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 export default function Performance() {
   return (
     <>
-        <div className='flex justify-center'>
-            <div className='w-1/5 p-10'>
-                <h3 className='font-medium text-5xl pb-4'>96%</h3>
-                <h4 className='w-4/5'>Those who view product/service videos are 96% more likely to buy</h4>
+        <div className='flex md:justify-center max-md:justify-between'>
+            <div className='lg:w-1/5 max-lg:w-3/12 max-md:w-[30%] md:p-10 max-md:px-4'>
+                <h3 className='md:font-medium max-md:font-semibold md:text-5xl max-md:text-3xl pb-4'>96%</h3>
+                <h4 className='md:w-4/5 max-md:text-xs'>Those who view product/service videos are 96% more likely to buy</h4>
             </div>
-            <div className='w-1/5 p-10 border-r-2 border-l-2'>
-                <h3 className='font-medium text-5xl pb-4'>#1</h3>
-                <h4 className='w-4/5'>Rated #1 in agencies who help small businesses grow. Among the worlds top 3%</h4>
+            <div className='lg:w-1/5 max-lg:w-3/12 max-md:w-[30%] md:p-10 max-md:px-4 border-r-2 border-l-2'>
+                <h3 className='md:font-medium max-md:font-semibold md:text-5xl max-md:text-3xl pb-4'>#1</h3>
+                <h4 className='md:w-4/5 max-md:text-xs'>Rated #1 in agencies who help small businesses grow. Among the worlds top 3%</h4>
             </div>
-            <div className='w-1/4 p-10'>
-                <h3 className='font-medium text-5xl pb-4'>78%</h3>
-                <h4 className='w-4/5'>78% of small businesses don't have the money to compete with big business top-tier talent. 90% of them don't know where to start...</h4>
+            <div className='lg:w-1/4 max-lg:w-4/12 max-md:w-[40%] md:p-10 max-md:px-4'>
+                <h3 className='md:font-medium max-md:font-semibold md:text-5xl max-md:text-3xl pb-4'>78%</h3>
+                <h4 className='md:w-4/5 max-md:text-xs'>78% of small businesses don't have the money to compete with big business top-tier talent. 90% of them don't know where to start...</h4>
             </div>
         </div>
     </>
