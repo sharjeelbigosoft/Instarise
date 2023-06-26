5ce9217987ef529cb7462d026bb82a4aaa6d31f7
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-26 15:46:46 +0500

    Services component made responsive

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,8 +4,8 @@ export default function Services() {
   return (
     <>
         <div className='grid my-8'>
-            <button className='capitalize justify-self-center lg:w-2/5 bg-black text-white py-2 rounded-lg'>Book a discovery call</button>
-            <div className='lg:w-2/5 justify-self-center flex justify-between my-10 text-md'>
+            <button className='capitalize justify-self-center lg:w-2/5 max-lg:w-3/5 max-md:w-4/5 bg-black text-white py-2 rounded-lg'>Book a discovery call</button>
+            <div className='lg:w-2/5 max-md:w-full max-md:px-4 max-lg:w-3/5 justify-self-center flex justify-between my-10 text-md'>
                 <span>Social Media Management</span>
                 <span>Web Design</span>
                 <span>TikTok</span>
