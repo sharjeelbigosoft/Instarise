6cdc01a308a5a1f9a17e30789ab70649468c840c
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-26 12:11:29 +0500

    Overview of services

---
+++ src/components/Services.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Services() {
+  return (
+    <>
+        <div className='grid my-8'>
+            <button className='capitalize justify-self-center lg:w-2/5 bg-black text-white py-2 rounded-lg'>Book a discovery call</button>
+            <div className='lg:w-2/5 justify-self-center flex justify-between my-10 text-md'>
+                <span>Social Media Management</span>
+                <span>Web Design</span>
+                <span>TikTok</span>
+            </div>
+        </div>
+    </>
+  )
+}
