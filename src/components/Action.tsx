be8f7f0a96c04e7e1507b8d5393dfc4deae91f2d
Sharjeel Ahmad <sharjeeljathol999@gmail.com>
2023-06-26 16:25:12 +0500

    Action component made responsive

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 export default function Action() {
   return (
     <>
-    <div className='w-full flex justify-center my-10'>
-        <div className='grid w-3/5 border rounded-md border-black justify-items-center justify-self-center py-12'>
-            <h3 className='text-2xl font-semibold'>You're here <span>&#9733;</span> Don't waste time. .</h3>
-            <h3 className='text-2xl font-semibold my-2'>Take Action.</h3>
-            <h3 className='text-2xl font-semibold uppercase'>Start Now</h3>
-            <p  className='py-4'>Schedule a 15 minute call with an expert. We will guide you in the right direction!</p>
-            <div className='flex w-7/12 justify-between'>
-                <button className='rounded-lg bg-black text-white py-2 px-16'>Book a Call</button>
-                <button className='rounded-lg bg-white text-black py-2 px-16 border-2'>See Instagram</button>
+    <div className='w-full flex justify-center my-10 px-4'>
+        <div className='grid xl:w-3/5 lg:w-4/5 max-md:w-full max-lg:w-[90%] border rounded-md border-black justify-items-center justify-self-center py-12'>
+            <h3 className='md:text-2xl max-md:text-xl md:font-semibold'>You're here <span>&#9733;</span> Don't waste time.</h3>
+            <h3 className='md:text-2xl max-md:text-xl md:font-semibold my-2'>Take Action.</h3>
+            <h3 className='md:text-2xl max-md:text-xl md:font-semibold uppercase'>Start Now</h3>
+            <p  className='py-4 max-md:text-sm max-md:px-6'>Schedule a 15 minute call with an expert. We will guide you in the right direction!</p>
+            <div className='flex md:w-[80%] max-md:w-full justify-center space-x-6 max-md:text-sm'>
+                <button className='rounded-lg bg-black text-white py-2 md:px-16 max-md:px-6 max-md:w-[40%] md:w-[50%]'>Book a Call</button>
+                <button className='rounded-lg bg-white text-black py-2 md:px-16 max-md:px-6 max-md:w-[40%] md:w-[50%] border-2'>See Instagram</button>
 
             </div>
         </div>
